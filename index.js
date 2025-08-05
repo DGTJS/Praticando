05055e5c393fdf76cbb1258b4850c7a251f4522a
DGTJS <joaodiegom15@gmail.com>
2025-08-05 10:35:55 -0300

    Praticando Extends e Super

--- index.js
+++ index.js
@@ -11,14 +11,24 @@ class guerreiro {
 }
 
 class Vikings extends guerreiro {
+  constructor(nome, idade, machado) {
+    super(nome, idade);
+    this.machado = machado;
+  }
   lutar() {
     console.log(
-      `O viking ${this.nome}, de ${this.idade} anos, está lutando com sua espada!`
+      `O viking ${this.nome}, de ${this.idade} anos, está lutando em nome de Odin!`
+    );
+  }
+  lutarComMachado() {
+    console.log(
+      `O viking ${this.nome}, de ${this.idade} anos, está lutando com seu machado ${this.machado}!`
     );
   }
 }
 
 guerreiro1 = new guerreiro("Gladiador", 28);
 guerreiro1.lutar();
-guerreiro2 = new Vikings("Ragnar", 35);
+guerreiro2 = new Vikings("Ragnar", 35, "Spartan Sword");
 guerreiro2.lutar();
+guerreiro2.lutarComMachado();
