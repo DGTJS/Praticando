b202a997e6c4f36047ea8159da339cba7949a3a4
DGTJS <joaodiegom15@gmail.com>
2025-08-07 10:11:34 -0300

    Praticando arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,9 @@
+const verificarPositivo = (Numero) => {
+  if (Numero > 0) {
+    console.log("O numero é positivo");
+  } else {
+    console.log("O numero é negativo");
+  }
+};
+
+verificarPositivo(-1);
